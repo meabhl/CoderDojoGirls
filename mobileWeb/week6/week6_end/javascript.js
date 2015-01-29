9f6e6ecc75403a74f9b3c1c494b8f62459ddb242
sazmeise <sdoran@gmail.com>
2015-01-29 20:55:48 +0000

    Adding the final code for week6: week6_end and the starting point for week7 (which will be overwritten with the changes later)

---
+++ mobileWeb/week6/week6_end/javascript.js
@@ -0,0 +1,13 @@
+
+
+//Change the style of the bird image to hide the picture
+function hideBird() {
+ bird.style.display="none";
+}
+
+//Change the style of the bird to show the picture
+function showBird() { 
+ bird.style.display="block";
+}
+
+
---
+++ mobileWeb/week7/README.md
@@ -0,0 +1,7 @@
+# Week 7 January 31st 2015
+This week we are going to learn more about animation!
+
+- How about we make the bird fly around in circles?
+--
+
+-
