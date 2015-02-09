8406b8924a85c3106d4621b28124cc48a90ed6b2
sazmeise <sdoran@gmail.com>
2015-02-09 21:37:01 +0000

    Week8_Niambh changes

--- mobileWeb/week8/javascript.js
+++ mobileWeb/week8/javascript.js
@@ -1,4 +1,47 @@
+onload=function(){
+ sapling.style.display="none";
+};
 
+
+
+var increaseBy=30;
+var counter=0;
+
+function grow(){
+
+  counter=counter+1;
+  
+  if(counter <3){
+	  increaseWidth();
+	  increaseHeight();
+  }
+}
+
+function increaseWidth(){
+  
+  twig = document.getElementById("twig");
+  
+  var width= parseInt(twig.offsetWidth);
+  
+ 
+  width=width+increaseBy;
+  
+  twig.style.width=width+"px";
+  
+}
+
+function increaseHeight(){
+  
+  twig = document.getElementById("twig");
+  
+  var height= parseInt(twig.offsetHeight);
+  
+ 
+  height=height+increaseBy;
+  
+  twig.style.height=height+"px";
+  
+}
 
 //Change the style of the bird image to hide the picture
 function hideBird() {
