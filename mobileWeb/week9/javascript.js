e21f00c133bfb01ec5b787342e46eb19f7bd93f1
sazmeise <sdoran@gmail.com>
2015-02-09 21:45:56 +0000

    Minor code reformat

--- mobileWeb/week9/javascript.js
+++ mobileWeb/week9/javascript.js
@@ -1,56 +1,44 @@
-onload=function(){
- sapling.style.display="none";
+onload = function () {
+    sapling.style.display = "none";
 };
 
+var increaseBy = 30;
+var counter = 0;
+var twig;
 
-
-var increaseBy=30;
-var counter=0;
-
-function grow(){
-
-  counter=counter+1;
-  
-  if(counter <3){
-	  increaseWidth();
-	  increaseHeight();
-  }
+function grow() {
+    counter = counter + 1;
+    if (counter < 3) {
+        increaseWidth();
+        increaseHeight();
+    }
 }
 
-function increaseWidth(){
-  
-  twig = document.getElementById("twig");
-  
-  var width= parseInt(twig.offsetWidth);
-  
- 
-  width=width+increaseBy;
-  
-  twig.style.width=width+"px";
-  
+function increaseWidth() {
+    twig = document.getElementById("twig");
+    var width = parseInt(twig.offsetWidth);
+
+    width = width + increaseBy;
+    twig.style.width = width + "px";
 }
 
-function increaseHeight(){
-  
-  twig = document.getElementById("twig");
-  
-  var height= parseInt(twig.offsetHeight);
-  
- 
-  height=height+increaseBy;
-  
-  twig.style.height=height+"px";
-  
+function increaseHeight() {
+    twig = document.getElementById("twig");
+
+    var height = parseInt(twig.offsetHeight);
+    height = height + increaseBy;
+
+    twig.style.height = height + "px";
 }
 
 //Change the style of the bird image to hide the picture
 function hideBird() {
- bird.style.display="none";
+    bird.style.display = "none";
 }
 
 //Change the style of the bird to show the picture
-function showBird() { 
- bird.style.display="block";
+function showBird() {
+    bird.style.display = "block";
 }
 
 
